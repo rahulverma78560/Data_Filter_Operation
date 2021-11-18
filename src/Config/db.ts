098ff49cf7fc1872498703f5992dba9eb262cab9
Rahul Kumar <rahulverma78560@gmail.com>
2021-11-18 19:25:19 +0530

    feat:(MongoDB:Setup done for MongoDB using Atlas)

---
+++ src/Config/db.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import chalk from "chalk";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(
+      "mongodb+srv://G7CR:G7CR@cluster0.l5qv2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" ||
+        process.env.MONGO_URI
+    );
+    console.log(
+      chalk.blueBright.bold(
+        `MongoDB conncected!!!!!!!!!: ${conn.connection.host}`
+      )
+    );
+  } catch (error) {
+    handleError(error);
+  }
+};
+function handleError(error: unknown) {
+  throw new Error("An error occured while connecting to MongoDB");
+}
+
+export default connectDB;
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,25 @@
 import express from "express";
 import * as dotenv from "dotenv";
+import connectDB from "./Config/db";
+import morgan from "morgan";
+import chalk from "chalk";
 
+connectDB();
 dotenv.config();
 const PORT = process.env.PORT || 3000;
 const app = express();
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.listen(PORT, () => {
-  console.log(`Example app listening at http://localhost:${PORT}`);
+  console.log(
+    chalk.yellow.bold(
+      `Server is running on port ${process.env.NODE_ENV} mode on port ${PORT}`
+    )
+  );
 });
