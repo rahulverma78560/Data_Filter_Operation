bfbebfa6ea24d2198539e799fe6ff705cdf9d177
Rahul Kumar <rahulverma78560@gmail.com>
2021-11-18 19:40:25 +0530

    feat:(MongoDB:Setup done for MongoDB using Atlas)

--- src/Config/db.ts
+++ src/Config/db.ts
@@ -4,8 +4,8 @@ import chalk from "chalk";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      "mongodb+srv://G7CR:G7CR@cluster0.l5qv2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" ||
-        process.env.MONGO_URI
+      process.env.MONGO_URI ||
+        "mongodb+srv://G7CR:G7CR@cluster0.l5qv2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     );
     console.log(
       chalk.blueBright.bold(
