8ca99c85cd418fc5f3f9c00e1c0858ab3678c1b2
Rahul Kumar <rahulverma78560@gmail.com>
2021-11-19 12:05:31 +0530

    bug(server.ts:Port bug fixed)

--- src/server.ts
+++ src/server.ts
@@ -4,15 +4,13 @@ import connectDB from "./Config/db";
 import morgan from "morgan";
 import chalk from "chalk";
 
-const PORT = process.env.PORT || 3000;
 const app = express();
-
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
-
 connectDB();
 dotenv.config();
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -21,7 +19,7 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(
     chalk.yellow.bold(
-      `Server is running on ${
+      `Server is running on port ${
         process.env.NODE_ENV || "Production"
       } mode on port ${PORT}`
     )
