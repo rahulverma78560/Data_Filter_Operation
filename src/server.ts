cf99d0206f7e089a40d006c09124647cc616d7e4
rakshith28 <rakshithballal213@gmail.com>
2021-11-22 18:50:23 +0530

    feat(GetData|Filter:getting data from csv file and filtering accoring to the formula )

--- src/server.ts
+++ src/server.ts
@@ -3,19 +3,22 @@ import * as dotenv from "dotenv";
 import connectDB from "./Config/db";
 import morgan from "morgan";
 import chalk from "chalk";
-
+import { addata, addc1, addc2} from "./Controller/controllers";
 const app = express();
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
-}
+} 
+
+const jsonParserMiddleware=express.json()
+app.use(jsonParserMiddleware)
+app.post("/",addata)
+app.post("/addc3",addc2)
+app.post("/addc1",addc1)
+
+
 connectDB();
 dotenv.config();
 const PORT = process.env.PORT || 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.listen(PORT, () => {
   console.log(
     chalk.yellow.bold(
