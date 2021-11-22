f56d8b5452d01bebcaa007a4cc1563eb63b61d6e
Darshan475 <darshankurubas761@gmail.com>
2021-11-22 13:45:52 +0530

    feat(JSON_Response:Added json response)

--- src/Controller/controllers.ts
+++ src/Controller/controllers.ts
@@ -1,5 +1,7 @@
 import { Data} from "../model/schema";
 import { Request,Response } from "express";
+import { createResponse } from "../Utility/response";
+
 export const dataController= async(req:Request,res:Response)=>{
     let data= new Data(req.body);
     await data.save((err:any)=>{
@@ -13,12 +15,7 @@ export const dataController= async(req:Request,res:Response)=>{
 }
 
 export const deleteController=async(req:Request,res:Response)=>{
-    try{  const process =await Data.deleteMany({"isProcessed":1})
-       if(process.deletedCount===0) 
-          return res.status(404).json("Documents not found") 
-   return res.status(200).json("Successfully Deleted")
-}
-    catch(e){
-        console.log(e)
-    }
+     const process =await Data.deleteMany({"isProcessed":1})
+       if(process.deletedCount===0) return res.status(404).json(createResponse(404,"Document not found"));
+   return res.status(200).json(createResponse(200,"Successfully Deleted"));
  }
