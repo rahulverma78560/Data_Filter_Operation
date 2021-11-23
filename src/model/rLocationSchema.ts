38008295cd714a38e3cf12a26cb319d4945af08b
Ganesh <69515925+Ganesh123-hub@users.noreply.github.com>
2021-11-23 11:03:14 +0530

    Update rLocationSchema.ts
    isCleaned key is added to cl1

--- src/model/rLocationSchema.ts
+++ src/model/rLocationSchema.ts
@@ -1,14 +1,16 @@
 import mongoose from "mongoose";
 
 // ------ colletion 1  (get records) ---------
-const cl1 = new mongoose.Schema({ });
+const cl1 = new mongoose.Schema({ 
+    isCleaned: {type: Number}
+});
 export let col1 = mongoose.model("db1", cl1);
 
 // -----Collection 2 (insert records)-------------
 const cl2 = new mongoose.Schema({
     SubscriptionID: { type: String },
     resourseLocation: { type: String },
-    totalEstimationPrice: { type: Number },
+    applicableEstimation: { type: Number },
 });
 export let col2 = mongoose.model("db2", cl2);
 
