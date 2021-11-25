f3c994779870ade38bebfa97cc1816b6b1081b31
Pratik Kulkarni <pratikkulkarni8686@gmail.com>
2021-11-25 13:02:24 +0530

    feat(updateData:handlled error)

--- src/Manager/updateManager.ts
+++ src/Manager/updateManager.ts
@@ -2,9 +2,7 @@ import { locationCollection } from "../model/Subs_Location_Schema";
 import { groupCollection } from "../model/Subs_group_Schema";
 
 export const updateData = async(Data:any, id:string, Applicable_Estimated_Charges:number)=>{
-    if(!Applicable_Estimated_Charges){
-        return Promise.reject("Cannot Modify the requested field")
-      }
+
       let update=await locationCollection.findByIdAndUpdate(id, {
         Applicable_Estimated_Charges: Data.Applicable_Estimated_Charges
       },
