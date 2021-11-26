48affbc9c12521331103fb021898b1ae5c744df4
Rahul Kumar <rahulverma78560@gmail.com>
2021-11-26 13:14:38 +0530

    Uploads:added upload feature

---
+++ src/Routes/uploadRoutes.ts
@@ -0,0 +1,38 @@
+import path from "path";
+import express from "express";
+import multer from "multer";
+const uploadRouter = express.Router();
+
+const storage = multer.diskStorage({
+  destination(req, file, cb) {
+    cb(null, "uploads/");
+  },
+  filename(req, file, cb) {
+    cb(null, `${file.fieldname}${path.extname(file.originalname)}`);
+  },
+});
+
+function checkFileType(file: any, cb: any) {
+  const filetypes = /csv/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb("CSV only!");
+  }
+}
+
+const upload = multer({
+  storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
+
+uploadRouter.post("/", upload.single("data"), (req, res) => {
+  res.send(`../uploads/`);
+});
+
+export default uploadRouter;
--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,10 @@ import * as dotenv from "dotenv";
 import connectDB from "./Config/db";
 import morgan from "morgan";
 import chalk from "chalk";
+import path from "path";
 import { autoPost, autoDelete } from "./Config/jobScheduler";
 import routerMiddleware from "./Routes/routes";
+import uploadRouter from "./Routes/uploadRoutes";
 
 const app = express();
 if (process.env.NODE_ENV === "development") {
@@ -13,6 +15,7 @@ if (process.env.NODE_ENV === "development") {
 const jsonParserMiddleware = express.json();
 app.use(jsonParserMiddleware);
 app.use(routerMiddleware);
+app.use(uploadRouter);
 autoPost.start();
 autoDelete.start();
 connectDB();
@@ -22,6 +25,9 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send(">>>>>>>>>>>> DataFilteration <<<<<<<<<<<<<");
 });
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 app.listen(PORT, () => {
   console.log(
