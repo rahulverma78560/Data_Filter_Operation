89ce33aeb811bc0cddffe7bc8872955e559111d5
Pratik Kulkarni <pratikkulkarni8686@gmail.com>
2021-11-26 17:24:17 +0530

    feat:(uploads:Posting Group and Location data to Database)

--- src/Routes/routes.ts
+++ src/Routes/routes.ts
@@ -5,10 +5,16 @@ import { groupDataHandler } from "../Controller/subGroupController";
 import { removeDataHandler } from "../Controller/deleteController";
 import { groupByLocationHandler } from "../Controller/subLocationController";
 import { downloadDataHandler } from "../Controller/downloadDataController";
+import { addGroupHandler } from "../Controller/postGroupControllers";
+import { addLocationHandler } from "../Controller/postLocationControllers";
 
 const routerMiddleware = Router();
 
 routerMiddleware.post("/rawCollection", addDataHandler);
+
+routerMiddleware.post("/updateGroup",addGroupHandler);
+
+routerMiddleware.post("/updateLocation",addLocationHandler);
 
 routerMiddleware.get("/location", groupByLocationHandler);
 
